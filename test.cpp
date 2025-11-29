0b712b857d601ab7181ef5c73d4bcee84d8a44d9
Yuval Kogan <yuvalkogan123@gmail.com>
2025-11-29 20:45:06 +0200

    Remove erdundant files

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "This is a test and practice on Github actions" << std::endl;
-    std::cout << "Make sure to delete this file right after a new C++ file is inserted into the project" << std::endl;
-
-    return 0;
-}
