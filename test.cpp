68030827b4e1c004a212abdb4ffa6982e104408e
Yuval Kogan <yuvalkogan123@gmail.com>
2025-11-29 19:06:29 +0200

    test

--- test.cpp
+++ test.cpp
@@ -4,4 +4,6 @@ int main()
 {
     std::cout << "This is a test and practice on Github actions" << std::endl;
     std::cout << "Make sure to delete this file right after a new C++ file is inserted into the project" << std::endl;
+
+    return 0;
 }
