b3e8836410a25e725b9c9235259a4e0cd5478e31
KoganTheDev <Yuvalkogan123@gmail.com>
2026-01-15 15:28:17 +0200

    refactor, clean code

--- src/ESP32/lib/detection/include/test_detection.h
+++ src/ESP32/lib/detection/include/test_detection.h
@@ -10,9 +10,6 @@
 
 class TestDetection : public BaseDetectionModule
 {
-private:
-    static inline int print_limit = 0;
-
 public:
     TestDetection() {}
     ~TestDetection() {}
@@ -26,53 +23,6 @@ public:
 
         MoveDirectionX random_number_x = MoveDirectionX((rand() % (max - min + 1)) + min);
         MoveDirectionY random_number_y = MoveDirectionY((rand() % (max - min + 1)) + min);
-
-        if (print_limit == 100)
-        {
-            print_limit = 0;
-
-            Serial.print("x movement: ");
-            switch (random_number_x)
-            {
-            case MoveDirectionX::None:
-                Serial.println("stay");
-                break;
-
-            case MoveDirectionX::Right:
-                Serial.println("right");
-                break;
-
-            case MoveDirectionX::Left:
-                Serial.println("left");
-                break;
-
-            default:
-                break;
-            }
-
-            Serial.print("y movement: ");
-            switch (random_number_y)
-            {
-            case MoveDirectionY::None:
-                Serial.println("stay");
-                break;
-
-            case MoveDirectionY::Up:
-                Serial.println("up");
-                break;
-
-            case MoveDirectionY::Down:
-                Serial.println("down");
-                break;
-
-            default:
-                break;
-            }
-
-            Serial.printf("Tuple returned (%d, %d)\n", random_number_x, random_number_y);
-        }
-
-        print_limit++;
 
         return std::make_tuple(random_number_x, random_number_y);
     };
